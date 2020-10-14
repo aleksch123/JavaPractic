772381dd80121829256bfd97d68bcf2b6d7acd20
AleksCh <ssss.utest@gmail.com>
2020-10-14 17:06:08 +0300

    ex2

---
+++ src/main/java/Day3ex2.java
@@ -0,0 +1,34 @@
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Day3ex2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(repeatFinder("Ну что ж я, я найти решения правильного " +
+                "не смогу ж? Смогу ж конечно, я ж старательный все ж таки.",3));
+
+
+    }
+
+    public static String repeatFinder(String sourse,int repeat){
+        JSONObject json = new JSONObject();
+        String formattedText =sourse.toLowerCase().replaceAll("^a-яa-z-","").replaceAll("\\.","");
+        List<String> words = Arrays.asList(formattedText.split(" "));
+        for (String word:words)
+        {
+            int frequency = Collections.frequency(words,word);
+            if (frequency>=repeat) {
+                json.put(word,frequency);
+            }
+        }
+
+        return String.valueOf(json);
+    }
+
+}
+
