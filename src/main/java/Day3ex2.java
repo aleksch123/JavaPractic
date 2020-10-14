4f5bce789a9af03ca43b8b07978eda7ecf34cd67
AleksCh <ssss.utest@gmail.com>
2020-10-14 20:30:10 +0300

    update regexp

--- src/main/java/Day3ex2.java
+++ src/main/java/Day3ex2.java
@@ -9,15 +9,15 @@ public class Day3ex2 {
 
     public static void main(String[] args) {
 
-        System.out.println(repeatFinder("Ну что ж я, я найти решения правильного " +
-                "не смогу ж? Смогу ж конечно, я ж старательный все ж таки.",3));
+            System.out.println(repeatFinder("Ну что ж я, я найти решения правильного не смогу ж? Смогу ж конечно, я ж старательный все ж таки.",3));
 
 
     }
 
     public static String repeatFinder(String sourseText,int maxFrequency){
         JSONObject json = new JSONObject();
-        String formattedText =sourseText.toLowerCase().replaceAll("^a-яa-z-","").replaceAll("\\.","");
+        String formattedText =sourseText.toLowerCase().replaceAll("[\\.,?!:]","");
+
         List<String> words = Arrays.asList(formattedText.split(" "));
         for (String word:words)
         {
