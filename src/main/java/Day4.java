480fd915dbf754322cfb842c34fe4681050e6b4f
AleksCh <ssss.utest@gmail.com>
2020-10-14 12:59:59 +0300

    День 4, задание 1

---
+++ src/main/java/Day4.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+        String before[]= new String []{"apple banana", "orange", "banana", "kiwi strawberry blueberry"};
+        String after[]=reformatArray(before);
+
+    }
+
+    public static String[] reformatArray(String[] words){
+        List<String> newWords;
+        List<String> newArray= new ArrayList<>();
+
+        for (String word: words){
+            newWords= Arrays.asList(word.split(" "));
+            newArray.addAll(newWords);
+        }
+        return newArray.toArray(new String[0]);
+
+        }
+
+    }
+
