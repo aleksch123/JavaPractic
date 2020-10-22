31ecea3cad749e12327e5146894d0045f7ed7975
AleksCh <ssss.utest@gmail.com>
2020-10-22 07:40:28 +0300

    ref

--- src/main/java/BinaryConverter.java
+++ src/main/java/BinaryConverter.java
@@ -1,15 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.lang.StrictMath.pow;
-import static java.lang.StrictMath.sqrt;
-
-
 public class BinaryConverter {
 
     public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <100 ; i++) {
+        for (int i = 1; i < 100; i++) {
 
             System.out.println(convertToBinary(i));
         }
@@ -17,15 +10,15 @@ public class BinaryConverter {
 
     }
 
-    private static String convertToBinary (int decimal){
-        String binary="";
-        while (decimal>0){
-            if (decimal%2 == 0) binary=binary+"0";
-            else binary=binary+"1";
-            decimal = decimal/2;
+    private static String convertToBinary(int decimal) {
+        String binary = "";
+        while (decimal > 0) {
+            if (decimal % 2 == 0) binary = binary + "0";
+            else binary = binary + "1";
+            decimal = decimal / 2;
         }
         return binary;
-        }
+    }
 
 }
 
