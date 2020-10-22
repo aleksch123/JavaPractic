c529a016100a030f1dc776dd871dcb79390176c6
AleksCh <ssss.utest@gmail.com>
2020-10-22 07:37:12 +0300

    ref

---
+++ src/main/java/BinaryConverter.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.StrictMath.pow;
+import static java.lang.StrictMath.sqrt;
+
+
+public class BinaryConverter {
+
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 1; i <100 ; i++) {
+
+            System.out.println(convertToBinary(i));
+        }
+
+
+    }
+
+    private static String convertToBinary (int decimal){
+        String binary="";
+        while (decimal>0){
+            if (decimal%2 == 0) binary=binary+"0";
+            else binary=binary+"1";
+            decimal = decimal/2;
+        }
+        return binary;
+        }
+
+}
+
+
